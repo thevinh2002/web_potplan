0eb7ebf23c94615d5bee8f55b9f5744d2e32543c
HarukiRaito <hatran12387@gmail.com>
2026-04-19 21:02:30 +0700

    fix: move key to env

--- src/libs/firebase.ts
+++ src/libs/firebase.ts
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAy0a5ZqgipUFklBZbf5Szl2qLqkIW_fJ8",
-  authDomain: "vad-pottery.firebaseapp.com",
-  projectId: "vad-pottery",
-  storageBucket: "vad-pottery.firebasestorage.app",
-  messagingSenderId: "624750345881",
-  appId: "1:624750345881:web:919223f0dfd14689904208",
-  measurementId: "G-RNVJ7W5TJV",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
